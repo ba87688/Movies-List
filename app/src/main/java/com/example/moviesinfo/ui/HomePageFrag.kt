7172bb97d23f3f09fd815a8d9fb89eb42df7d815
Jagi <ba87688@gmail.com>
2022-06-03 06:06:45 -0500

    adding viewbidning and fragments

---
+++ app/src/main/java/com/example/moviesinfo/ui/HomePageFrag.kt
@@ -0,0 +1,9 @@
+package com.example.moviesinfo.ui
+
+import androidx.fragment.app.Fragment
+import com.example.moviesinfo.R
+
+class HomePageFrag :Fragment(R.layout.home_fragment) {
+
+
+}
---
+++ app/src/main/java/com/example/moviesinfo/ui/SearchPageFrag.kt
@@ -0,0 +1,9 @@
+package com.example.moviesinfo.ui
+
+import androidx.fragment.app.Fragment
+import com.example.moviesinfo.R
+
+class SearchPageFrag :Fragment(R.layout.search_fragment) {
+
+
+}
